27f281b2ba3194b59b16a04043530b01aa6ecd09
alexsis-valentinovich <alexsis-valentinovich@gmail.com>
2022-07-21 21:42:36 +0500

    Добавьте файлы проекта.

---
+++ Part_1/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_1
+{
+    class Ugoll
+    {
+        double graduS;
+        double minutE;
+        double seconD;
+        public double Gradus
+        {
+            get
+            {
+                return graduS;
+            }
+            set
+            {
+                if (value > 360)
+                {
+                    graduS = value % 360;
+                }
+                else
+                {
+                    graduS = value;
+                }
+            }
+        }
+        public double Minute
+        {
+            get
+            {
+                return minutE;
+            }
+            set
+            {
+                graduS += Math.Truncate(value / 60);
+                minutE = value % 60;
+            }
+        }
+        public double Second
+        {
+            get
+            {
+                return seconD;
+            }
+            set
+            {
+                minutE += Math.Truncate(value / 60);
+                seconD = value % 60;
+            }
+        }
+        public Ugoll(double graduS, double minutE, double seconD)
+        {
+            this.Gradus = graduS;
+            this.Minute = minutE;
+            this.Second = seconD;
+            //Gradus = gradus;
+            // Minute = minute;
+            // Second = second;
+        }
+        public double ToRadians()
+        {
+            return graduS * Math.PI / 180 + minutE / 60 * Math.PI / 180 + seconD / 3600 * Math.PI / 180;
+        }
+    }
+}
---
+++ Part_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//1.   Угол задан с помощью целочисленных значений gradus - градусов, min - угловых минут, sec - угловых секунд. Реализовать класс,
+//в котором указанные значения представлены в виде свойств. Для свойств предусмотреть проверку корректности данных. Класс должен
+//содержать конструктор для установки начальных значений, а также метод ToRadians для перевода угла в радианы. Создать объект на
+//основе разработанного класса. Осуществить использование объекта в программе.
+namespace Part_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение угла");
+            Console.WriteLine("Градусы");
+            double gradus = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Минуты");
+            double minute = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Секунды");
+            double second = Convert.ToDouble(Console.ReadLine());
+            Ugoll ugoll = new Ugoll(gradus, minute, second);
+            double gradusRadian = ugoll.ToRadians();
+            Console.WriteLine("Значение угла в радианах = {0:0.0000}", gradusRadian);
+            Console.ReadLine();
+        }
+
+    }
+}
